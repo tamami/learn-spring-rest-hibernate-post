5b25586da3671e68fdeabe44faa00f8b53ce720b
tamami <tamami.oka@gmail.com>
2016-11-08 15:00:20 +0700

    update content

---
+++ src/main/java/lab/aikibo/controller/SpptRestController.java
@@ -0,0 +1,31 @@
+package lab.aikibo.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by tamami on 08/11/16.
+ */
+public class SpptRestController {
+
+    @Autowired
+    SpptServices spptServices;
+
+    @Autowired
+    PembayaranServices pembayaranServices;
+
+    @Autowired
+    ReversalServices reversalServices;
+
+    static final Logger logger = Logger.getLogger(SpptRestController.class);
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public Status login()
+
+}
