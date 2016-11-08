bfaee2d0eac5d26971132c04f65eb06fb17b3370
tamami <tamami.oka@gmail.com>
2016-11-08 09:16:08 +0700

    add method bonecp data source

--- src/main/java/lab/aikibo/config/HibernateConfig.java
+++ src/main/java/lab/aikibo/config/HibernateConfig.java
@@ -1,7 +1,55 @@
 package lab.aikibo.config;
 
+import com.jolbox.bonecp.BoneCPDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+
+import java.util.Properties;
+
 /**
  * Created by tamami on 07/11/16.
  */
 public class HibernateConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(boneCPDataSource());
+        sessionFactory.setPackagesToScan(new String[] {"lab.aikibo.model"});
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    private Properties hibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        properties.put("hibernate.hbm2ddl.auto", "validate");
+        return properties;
+    }
+
+    @Bean
+    public BoneCPDataSource boneCPDataSource() {
+        BoneCPDataSource ds = new BoneCPDataSource();
+        ds.setDriverClass(environment.getRequiredProperty("jdbc.driverClassName"));
+        ds.setJdbcUrl(environment.getRequiredProperty("jdbc.url"));
+        ds.setUsername(environment.getRequiredProperty("jdbc.username"));
+        ds.setPassword(environment.getRequiredProperty("jdbc.password"));
+
+        ds.setIdleConnectionTestPeriodInMinutes(60);
+        ds.setIdleMaxAgeInMinutes(420);
+        ds.setMaxConnectionsPerPartition(30);
+        ds.setPartitionCount(10);
+        ds.setAcquireIncrement(5);
+        ds.setStatementsCacheSize(100);
+        ds.setReleaseHelperThreads(3);
+        return ds;
+    }
+
 }
